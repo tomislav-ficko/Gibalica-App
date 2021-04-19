06687f3966924c048d075ed370524249fe1b8405
Tomislav Ficko <tomislav.ficko@fer.hr>
2021-04-19 12:33:51 +0200

    Extract image analyzer into separate class

--- app/src/main/java/hr/fer/tel/gibalica/ui/TrainingActivity.kt
+++ app/src/main/java/hr/fer/tel/gibalica/ui/TrainingActivity.kt
@@ -1,7 +1,6 @@
 package hr.fer.tel.gibalica.ui
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.graphics.Matrix
 import android.graphics.SurfaceTexture
@@ -13,15 +12,10 @@ import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.pose.Pose
-import com.google.mlkit.vision.pose.PoseDetection
-import com.google.mlkit.vision.pose.PoseDetector
-import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
 import dagger.hilt.android.AndroidEntryPoint
 import hr.fer.tel.gibalica.base.BaseActivity
 import hr.fer.tel.gibalica.databinding.ActivityTrainingBinding
-import hr.fer.tel.gibalica.utils.BodyPositions
+import hr.fer.tel.gibalica.utils.ImageAnalyzer
 import timber.log.Timber
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -113,7 +107,7 @@ class TrainingActivity : BaseActivity(), TextureView.SurfaceTextureListener {
                 val imageAnalyzer = ImageAnalysis.Builder()
                     .build()
                     .also {
-                        it.setAnalyzer(cameraExecutor, BodyAnalyzer())
+                        it.setAnalyzer(cameraExecutor, ImageAnalyzer())
                     }
                 cameraProvider.unbindAll()
 
@@ -172,38 +166,4 @@ class TrainingActivity : BaseActivity(), TextureView.SurfaceTextureListener {
             this,
             Manifest.permission.CAMERA
         ) == PackageManager.PERMISSION_GRANTED
-
-    inner class BodyAnalyzer() : ImageAnalysis.Analyzer {
-
-        @SuppressLint("UnsafeExperimentalUsageError")
-        override fun analyze(imageProxy: ImageProxy) {
-            val mediaImage = imageProxy.image
-            if (mediaImage != null) {
-                val image =
-                    InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-                val poseDetector = preparePoseDetector()
-                poseDetector.process(image)
-                    .addOnSuccessListener { it?.let { calculateDetectedPose(it) } }
-                    .addOnFailureListener { Timber.d("Detection failed: $it") }
-            }
-        }
-
-        private fun calculateDetectedPose(pose: Pose) {
-            when (BodyPositions.getPose(pose)) {
-                BodyPositions.NONE -> Timber.d("Pose could not be detected")
-                BodyPositions.SQUAT -> Timber.d("Squat detected")
-                BodyPositions.T_POSE -> Timber.d("T pose detected")
-                BodyPositions.LEFT_HAND_RAISED -> Timber.d("Left hand raised detected")
-                BodyPositions.RIGHT_HAND_RAISED -> Timber.d("Right hand raised detected")
-                BodyPositions.BOTH_HANDS_RAISED -> Timber.d("Both hands raised detected")
-            }
-        }
-
-        private fun preparePoseDetector(): PoseDetector {
-            val detectionOptions = PoseDetectorOptions.Builder()
-                .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
-                .build()
-            return PoseDetection.getClient(detectionOptions)
-        }
-    }
 }
---
+++ app/src/main/java/hr/fer/tel/gibalica/utils/ImageAnalyzer.kt
@@ -0,0 +1,51 @@
+package hr.fer.tel.gibalica.utils
+
+import android.annotation.SuppressLint
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.Pose
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.PoseDetector
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
+import timber.log.Timber
+
+class ImageAnalyzer() : ImageAnalysis.Analyzer {
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image =
+                InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            val poseDetector = preparePoseDetector()
+            poseDetector.process(image)
+                .addOnSuccessListener {
+                    it?.let {
+                        Timber.d("Detecting pose...")
+                        calculateDetectedPose(it)
+                    }
+                }
+                .addOnFailureListener { Timber.d("Detection failed: $it") }
+        }
+        imageProxy.close()
+    }
+
+    private fun calculateDetectedPose(pose: Pose) {
+        when (BodyPositions.getPose(pose)) {
+            BodyPositions.NONE -> Timber.d("Pose could not be detected")
+            BodyPositions.SQUAT -> Timber.d("Squat detected")
+            BodyPositions.T_POSE -> Timber.d("T pose detected")
+            BodyPositions.LEFT_HAND_RAISED -> Timber.d("Left hand raised detected")
+            BodyPositions.RIGHT_HAND_RAISED -> Timber.d("Right hand raised detected")
+            BodyPositions.BOTH_HANDS_RAISED -> Timber.d("Both hands raised detected")
+        }
+    }
+
+    private fun preparePoseDetector(): PoseDetector {
+        val detectionOptions = PoseDetectorOptions.Builder()
+            .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+            .build()
+        return PoseDetection.getClient(detectionOptions)
+    }
+}
