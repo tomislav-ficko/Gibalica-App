0df5f6bf99219ea84e66e4b5fdbc5ccedbc0642d
Tomislav Ficko <tomislav.ficko@fer.hr>
2021-06-22 12:49:52 +0200

    Change values to allow more time for correct analysis

--- app/src/main/java/hr/fer/tel/gibalica/utils/Constants.kt
+++ app/src/main/java/hr/fer/tel/gibalica/utils/Constants.kt
@@ -5,13 +5,13 @@ object Constants {
     // Amount of time in Competition mode between moving on to next pose
     const val DETECTION_INTERVAL_COMPETITION_MILLIS_EASY = 8000L
     const val DETECTION_INTERVAL_COMPETITION_MILLIS_MEDIUM = 5000L
-    const val DETECTION_INTERVAL_COMPETITION_MILLIS_HARD = 2000L
+    const val DETECTION_INTERVAL_COMPETITION_MILLIS_HARD = 3000L
 
     // Amount of time between analyzing pose data
     const val DETECTION_TIMEOUT_MILLIS_DEFAULT = 1000L
 
     // Amount of time in Day-Night mode between moving on to next pose
-    const val DETECTION_INTERVAL_DAY_NIGHT_MILLIS_EASY = 4000L
-    const val DETECTION_INTERVAL_DAY_NIGHT_MILLIS_MEDIUM = 3000L
-    const val DETECTION_INTERVAL_DAY_NIGHT_MILLIS_HARD = 2000L
+    const val DETECTION_INTERVAL_DAY_NIGHT_MILLIS_EASY = 5000L
+    const val DETECTION_INTERVAL_DAY_NIGHT_MILLIS_MEDIUM = 4000L
+    const val DETECTION_INTERVAL_DAY_NIGHT_MILLIS_HARD = 3000L
 }
